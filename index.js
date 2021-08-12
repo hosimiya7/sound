7aac9602baa62ef13481f7457790febe46133a25
hosimiya7 <nerokarurin@gmail.com>
2021-08-13 01:12:57 +0900

    スクロール対応とリファクタリング

--- index.js
+++ index.js
@@ -3,29 +3,57 @@
 
 // window.onload = function() {
 //     // 実行したい処理
-//     if (confirm('音が流れます。大丈夫ですか？')) {
+//     if (confirm('音量ボタンをonにすると、音が流れより当サイト楽しめます')) {
 //         console.log('OKボタンが押されました。')
-//         console.log($music)
-//         $music.play()
+
 //     } else {
 //         console.log('キャンセルボタンが押されました。')
 //     }
 // }
-let el = document.getElementById("water_music");
 
-function enableMute() {
-  el.muted = true;
-  console.log(el.muted)
-}
-
-function disableMute() {
-  el.muted = false;
-  console.log(el.muted)
-  el.play()
-}
-
-$(function(){
-  $('#Audio-Control button').click(function(){
+$(function () {
+  $('#Audio-Control button').click(function () {
     $('#Audio-Control button').toggleClass('active');
   });
+
+  $(window).scroll(function () {
+    if(disableMute)
+    if ($(this).scrollTop() > 100 && $(this).scrollTop() < 600) {
+
+
+    } else {
+
+    }
+  });
 });
+
+class Sound{
+
+  dom
+
+  constructor(id) {
+    this.dom = document.getElementById(id);
+  }
+
+  enableMute() {
+    this.dom.muted = true;
+    // console.log(this.dom.muted)
+  }
+
+  disableMute() {
+    this.dom.muted = false;
+    // console.log(this.dom.muted)
+  }
+
+  playSound() {
+    this.dom.play()
+  }
+
+  pauseSound() {
+    this.dom.pause()
+  }
+}
+
+
+
+
